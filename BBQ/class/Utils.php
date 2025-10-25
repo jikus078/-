84211eb0ccbdb73c69d1c9b311e3a6eb998c8143
jikus078 <jikus078@gmail.com>
2025-10-25 18:44:53 +0700

    Delete BBQ directory

--- BBQ/class/Utils.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    class Bootstrap {
-        public function displayAlert($message, $type) {
-            echo '<div class="alert alert-'. $type .'" role="alert">';
-            echo ''. $message .'';
-            echo '</div>';
-        }
-    }
-?>
